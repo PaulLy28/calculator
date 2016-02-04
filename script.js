815770b7fca35d61d6784243112cad32a939833d
Paul Ly <28ly.paul@gmail.com>
2016-02-04 10:53:01 -0800

    new save

--- script.js
+++ script.js
@@ -12,11 +12,14 @@ function callback(type,value,item){
 var my_calculator = new calculator(callback);
 
 
-//click handlers added to buttons
+//jQuery document onload function
 $(document).ready(function(){
+//jQuery buttons click function
     $("button").click(function(){
+//var name val is equal to "$(this)" which is referring to the button click function ".text()" which is the text in that button tag
         var val =$(this).text();
-        if (val == "CE") {
+//if statement with the condition: if variable val (defined above) is equal in value to string "CE" OR variable val is equal in value to string "C", (if true) the block of code to be executed will clear the display. ELSE (if false) the value will be added to the display
+        if (val == "CE" || val == "C") {
             my_calculator.allClear();
         }
         else {
